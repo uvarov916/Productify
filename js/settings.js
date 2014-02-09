6ef953e82fcb8305f0da16581519737195a4858a
Ivan Uvarov <tonk2012@gmail.com>
2014-02-09 06:16:36 -0500

    Finished settings JS file

--- js/settings.js
+++ js/settings.js
@@ -10,10 +10,22 @@ $(function () {
 		$('select option.first').after(temp);
 	}
 
+	var keys = Object.keys(localStorage);
+	for (var i = 0; i < keys.length; i++) {
+		if (keys[i].indexOf('_goal') !== -1) {
+			$('.no_goals').hide();
+			var time = Math.floor(parseInt(localStorage[keys[i]]) / 3600000);
+			var name = keys[i].substring(0, keys[i].indexOf('_goal'));
+			$('.right h3').after('<span><b>' + name + '</b> set to <b>' + time +'</b> hour(s)')
+		}
+	}
+
 	$('#submit').click(function () {
-		var name = $('#choose_category').val() + _goal;
+		var name = $('#choose_category').val() + "_goal";
 		var time = parseInt($('#num_hours').val() * 1000 * 3600);
-		localStorage[name] = time;
+		if (time !== 0) {
+			localStorage[name] = time;
+		}
 	});
 
 
